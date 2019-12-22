c6ad24e5fa2238510902ecc27881ce8fb61afdf9
Nand.HE <hyfspm@outlook.com>
2019-12-23 04:02:04 +0800

    Needed files

---
+++ Demo_Main/Install_NeededKit.bat
@@ -0,0 +1,7 @@
+pip install cffi==1.12.3
+pip install gevent==1.4.0
+pip install greenlet==0.4.15
+pip install pycparser==2.19
+pip install six==1.12.0
+pip install websocket==0.2.1
+pip install websocket-client==0.56.0
